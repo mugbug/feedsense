c14829b4c22d28324497316a1a856e872a40de8b
Pedro Zaroni <zaronipedro@outlook.com>
2018-08-12 17:09:21 -0300

    Push failing unit tests

---
+++ app/src/main/java/com/example/pedro/feedsense/modules/home/HomeViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.pedro.feedsense.modules.home
+
+import android.arch.lifecycle.ViewModel
+import com.example.pedro.feedsense.repository.RetrofitInitializer
+import rx.Observable
+
+class HomeViewModel {
+
+    fun printSomething() {
+        System.err.println("Hello")
+    }
+}
