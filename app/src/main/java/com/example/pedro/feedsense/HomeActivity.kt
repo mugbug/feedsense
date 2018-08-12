88807f4dc276abbe67bf84364449e8b42b5aade3
Pedro Zaroni <zaronipedro@outlook.com>
2018-08-11 21:54:15 -0300

    Add basic layout

--- app/src/main/java/com/example/pedro/feedsense/HomeActivity.kt
+++ app/src/main/java/com/example/pedro/feedsense/HomeActivity.kt
@@ -2,11 +2,13 @@ package com.example.pedro.feedsense
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.activity_home)
     }
 }
